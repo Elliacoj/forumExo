920137e4d045ee1cd8ac6c00418202feded7245d
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-10-06 16:03:44 +0200

    RoleManager and UserRoleManager finish

---
+++ Model/Manager/RoleManager.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Model\Manager;
+
+
+use App\Model\Entity\Role;
+use App\Model\Manager\Traits\TraitsManager;
+
+class RoleManager {
+    use TraitsManager;
+
+    /**
+     * Add a data into Role table
+     * @param Role $role
+     * @return bool
+     */
+    public function add(Role $role):bool {
+        $name = $role->getName();
+
+        $data = ["name" => $name];
+
+        return ObjectManager::add("INSERT INTO ellia_role (name) VALUES(:name)", $data);
+    }
+
+    /**
+     * Update a data into Role table
+     * @param Role $role
+     * @return bool
+     */
+    public function update(Role $role):bool {
+        $id = $role->getId();
+        $name = $role->getName();
+
+        $data = ["name" => $name, "id" => $id];
+
+        return ObjectManager::update("UPDATE ellia_role SET name = :name WHERE id = :id", $data);
+    }
+
+    /**
+     * Delete a data into Role table
+     * @param $id
+     * @return bool
+     */
+    public function delete($id): bool {
+        return ObjectManager::delete("DELETE FROM ellia_role WHERE id = '$id'");
+    }
+
+    /**
+     * Return a role
+     * @param $id
+     * @return Object
+     */
+    public function search($id):Object {
+        return ObjectManager::search("SELECT * FROM ellia_role WHERE id = '$id'", Role::class);
+    }
+
+    /**
+     * Return a table of all role
+     * @return array
+     */
+    public function get():array {
+        return ObjectManager::get("SELECT * FROM ellia_role", Role::class);
+    }
+}
---
+++ Model/Manager/UserRoleManager.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\Model\Manager;
+
+
+use App\Model\Entity\UserRole;
+use App\Model\Manager\Traits\TraitsManager;
+
+class UserRoleManager {
+    use TraitsManager;
+
+    /**
+     * Add a data into UserRole table
+     * @param UserRole $userRole
+     * @return bool
+     */
+    public function add(UserRole $userRole):bool {
+        $userFk = $userRole->getUserFk()->getId();
+        $roleFk = $userRole->getRoleFk()->getId();
+
+        $data = ["userFk" => $userFk, "roleFk" => $roleFk];
+
+        return ObjectManager::add("INSERT INTO ellia_user_role (user_fk, role_fk) VALUES(:userFk, :roleFk)", $data);
+    }
+
+    /**
+     * Update a data into UserRole table
+     * @param UserRole $userRole
+     * @return bool
+     */
+    public function update(UserRole $userRole):bool {
+        $id = $userRole->getId();
+        $userFk = $userRole->getUserFk()->getId();
+        $roleFk = $userRole->getRoleFk()->getId();
+
+        $data = ["userFk" => $userFk, "roleFk" => $roleFk, "id" => $id];
+
+        return ObjectManager::update("UPDATE ellia_user_role SET name = :name WHERE id = :id", $data);
+    }
+
+    /**
+     * Delete a data into UserRole table
+     * @param $id
+     * @return bool
+     */
+    public function delete($id): bool {
+        return ObjectManager::delete("DELETE FROM ellia_user_role WHERE id = '$id'");
+    }
+
+    /**
+     * Return a userRole
+     * @param $userFk
+     * @return Object
+     */
+    public function searchUser($userFk):Object {
+        return ObjectManager::search("SELECT * FROM ellia_user_role WHERE user_fk = '$userFk'", UserRole::class);
+    }
+
+    /**
+     * Return a table of all userRole
+     * @return array
+     */
+    public function get():array {
+        return ObjectManager::get("SELECT * FROM ellia_user_role", UserRole::class);
+    }
+}
