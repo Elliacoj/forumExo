5c5c596296cfcb10799aaad552758635b576d116
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-10-06 14:41:14 +0200

    token entity finish

---
+++ Model/Entity/Token.php
@@ -0,0 +1,95 @@
+<?php
+
+
+namespace App\Model\Entity;
+
+
+class Token {
+    private ?int $id;
+    private ?string $token;
+    private ?User $userFk;
+    private ?string $datetime;
+
+    /**
+     * Token constructor.
+     * @param int|null $id
+     * @param string|null $token
+     * @param User|null $userFk
+     * @param string|null $datetime
+     */
+    public function __construct(int $id = null, string $token = null, User $userFk = null, string $datetime = null)
+    {
+        $this->id = $id;
+        $this->token = $token;
+        $this->userFk = $userFk;
+        $this->datetime = $datetime;
+    }
+
+    /**
+     * Return the id of Token
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return the token of Token
+     * @return string|null
+     */
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set the token of Token
+     * @param string|null $token
+     * @return Token
+     */
+    public function setToken(?string $token): Token
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Return the user fk of Token
+     * @return User|null
+     */
+    public function getUserFk(): ?User
+    {
+        return $this->userFk;
+    }
+
+    /**
+     * set the user fk of Token
+     * @param User|null $userFk
+     * @return Token
+     */
+    public function setUserFk(?User $userFk): Token
+    {
+        $this->userFk = $userFk;
+        return $this;
+    }
+
+    /**
+     * Return the datetime of Token
+     * @return string|null
+     */
+    public function getDatetime(): ?string
+    {
+        return $this->datetime;
+    }
+
+    /**
+     * Set the datetime of Token
+     * @param string|null $datetime
+     * @return Token
+     */
+    public function setDatetime(?string $datetime): Token
+    {
+        $this->datetime = $datetime;
+        return $this;
+    }
+}
