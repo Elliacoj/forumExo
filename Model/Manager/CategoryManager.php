e632a506fc7c67c2a5dd25fd29dd3c53b01ea807
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-10-06 16:13:39 +0200

    CategoryManager and TokenManager finish

---
+++ Model/Manager/CategoryManager.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Model\Manager;
+
+
+use App\Model\Entity\Category;
+use App\Model\Manager\Traits\TraitsManager;
+
+class CategoryManager {
+    use TraitsManager;
+
+    /**
+     * Add a data into Category table
+     * @param Category $category
+     * @return bool
+     */
+    public function add(Category $category):bool {
+        $name = $category->getName();
+
+        $data = ["name" => $name];
+
+        return ObjectManager::add("INSERT INTO ellia_category (name) VALUES(:name)", $data);
+    }
+
+    /**
+     * Update a data into Category table
+     * @param Category $category
+     * @return bool
+     */
+    public function update(Category $category):bool {
+        $id = $category->getId();
+        $name = $category->getName();
+
+        $data = ["name" => $name, "id" => $id];
+
+        return ObjectManager::update("UPDATE ellia_category SET name = :name WHERE id = :id", $data);
+    }
+
+    /**
+     * Delete a data into Category table
+     * @param $id
+     * @return bool
+     */
+    public function delete($id): bool {
+        return ObjectManager::delete("DELETE FROM ellia_category WHERE id = '$id'");
+    }
+
+    /**
+     * Return a category
+     * @param $id
+     * @return Object
+     */
+    public function search($id):Object {
+        return ObjectManager::search("SELECT * FROM ellia_category WHERE id = '$id'", Category::class);
+    }
+
+    /**
+     * Return a table of all categories
+     * @return array
+     */
+    public function get():array {
+        return ObjectManager::get("SELECT * FROM ellia_category", Category::class);
+    }
+}
---
+++ Model/Manager/TokenManager.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace App\Model\Manager;
+
+
+use App\Model\Entity\Token;
+use App\Model\Manager\Traits\TraitsManager;
+
+class TokenManager {
+    use TraitsManager;
+
+    /**
+     * Add a data into Token table
+     * @param Token $token
+     * @return bool
+     */
+    public function add(Token $token):bool {
+        $userFk = $token->getUserFk()->getId();
+        $token = $token->getToken();
+
+        $data = ["token" => $token, "userFk" => $userFk];
+
+        return ObjectManager::add("INSERT INTO ellia_token (user_fk, token) VALUES(:userFk, :token)", $data);
+    }
+
+    /**
+     * Update a data into Token table
+     * @param Token $token
+     * @return bool
+     */
+    public function update(Token $token):bool {
+        $id = $token->getId();
+        $userFk = $token->getUserFk()->getId();
+        $token = $token->getToken();
+
+        $data = ["token" => $token, "userFk" => $userFk, "id" => $id];
+
+        return ObjectManager::update("UPDATE ellia_token SET user_fk = :userFk, token = :token WHERE id = :id", $data);
+    }
+
+    /**
+     * Delete a data into Token table
+     * @param $id
+     * @return bool
+     */
+    public function delete($id): bool {
+        return ObjectManager::delete("DELETE FROM ellia_token WHERE id = '$id'");
+    }
+
+    /**
+     * Return a token
+     * @param $token
+     * @param $userFk
+     * @return Object
+     */
+    public function searchToken($token, $userFk):Object {
+        return ObjectManager::search("SELECT * FROM ellia_token WHERE token = '$token' AND user_fk = '$userFk'", Token::class);
+    }
+
+    /**
+     * Return a table of all token
+     * @return array
+     */
+    public function get():array {
+        return ObjectManager::get("SELECT * FROM ellia_token", Token::class);
+    }
+}
